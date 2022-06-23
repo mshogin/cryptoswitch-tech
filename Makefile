06a8430aa52303d9c3d67d985e21794487814115
Mikhail Shogin <mshogin@gmail.com>
2022-06-23 23:43:22 +0200

    wip

--- Makefile
+++ Makefile
@@ -1,37 +1,5 @@
-GOCMD=$(CURDIR)/cmd
-
-.PHONY: init build clean
-
-all: build
-
-# CryptoSwitch
-# WEB
-web=35.214.191.249
-
-web-box-create:
-	ssh web sudo apt update
-	ssh web sudo apt install -y nginx
-	ssh web sudo useradd -s /bin/bash -d /home/igor/ -m -G sudo igor
-	ssh web sudo useradd -s /bin/bash -m web
-	ssh web sudo chown -R web:web /var/www/html
-	ssh web sudo chmod -R 0775 /var/www/html
-	ssh web sudo usermod -a -G web mshogin
-	ssh web sudo usermod -a -G web ikopanev
-
-web-run-dev:
-	npm run dev
-
-web-build:
-	npm run build
-
-web-deploy:
-	/usr/bin/rsync -a ./www/ $(web):~/www
+run:
 	ssh web sudo rm -rf /var/www/html
-	ssh web sudo cp -rf ~/www /var/www/html
+	ssh web sudo cp -rf ~/web-ui/www /var/www/html
 	ssh web sudo chown -R web:web /var/www/html
 	ssh web sudo systemctl restart nginx
-
-web-deploy-nginx:
-	scp sc-web-nginx.conf web:~/
-	ssh web sudo cp -f ~/sc-web-nginx.conf /etc/nginx/sites-enabled/
-	ssh web sudo systemctl restart nginx.service
--- src/App.vue
+++ src/App.vue
@@ -1,148 +1,134 @@
 <template>
-    <trading-vue
-        :data="chart"
-        :width="this.width"
-        :height="this.height"
-        skin="darkside"
-        :toolbar="true"
-    >
-    </trading-vue>
+    <!-- TradingVueJs 101 (example from 'Getting Started' ) -->
 
+    <trading-vue :data="chart" :width="this.width" :height="this.height" skin="darkside">
+
+</trading-vue>
 </template>
 
 <script>
 import {TradingVue, DataCube} from "trading-vue-js";
+import Data from '../data/data.json'
 
 export default {
-    name: "app",
+    name: 'app',
     components: { TradingVue },
-    props: {
-        titleTxt: {
-            type: String,
-            "default": 'CryptoSwitch'
+    strategy: ["alpha", "beta"],
+    methods: {
+        onResize(event) {
+            this.width = window.innerWidth
+            this.height = window.innerHeight
+        },
+        async fetchHistoricalData() {
+            try {
+                let rawResponse = await fetch("/api/v1/historicaldata");
+                let response = await rawResponse.json()
+                console.log("historicaldata", response.status)
+                if (response.status == 'success') {
+                    this.chart.data.chart = this.createCandlesChart(response.data)
+                    this.updateTicker();
+                    this.fetchOrderHistory()
+                } else {
+                    console.log("fetchHistoricalData.FAILED")
+                }
+                return []
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        createCandlesChart(data) {
+            return {type: "Candles",data: data}
+        },
+        async fetchOrderHistory() {
+            try {
+                let rawResponse = await fetch("/api/v1/orderhistory");
+                let response = await rawResponse.json()
+                console.log("orderhistory", response.status)
+                if (response.status == 'success') {
+                    this.chart.data.onchart = [
+                        this.createTradesChart(response.data)
+                    ]
+                } else {
+                    console.log("fetchOrderHistory.FAILED")
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        createTradesChart(data){
+            return {
+                name: "Trades",
+                type: "Trades",
+                data: data,
+                settings: {
+                    "legend": true,
+                    "z-index": 1,
+                    showLabel: true,
+                    markerSize: 20
+                }
+            }
+        },
+
+        async updateTicker() {
+            setInterval(async () => {
+                try {
+                    let rawResponseCandle = await fetch("/api/v1/lastcandle");
+                    let rawResponseTrade = await fetch("/api/v1/lasttrade");
+                    var responseCandel = await rawResponseCandle.json()
+                    var responseTrade = await rawResponseTrade.json()
+
+                    // console.log("lastcandle: ", responseCandel)
+                    // console.log("lasttrade: ", responseTrade)
+
+                    if (responseTrade.status != 'success') {
+                        console.log("updateTicker.FAILED(last-trade)")
+                        return
+                    }
+                    if (responseCandel.status != 'success') {
+                        console.log("updateTicker.FAILED(last-cande)")
+                        return
+                    }
+                    let lastCandle = responseCandel.data[0]
+                    let lastTrade = responseTrade.data[0]
+                    lastCandle[4] = lastTrade[1]
+                    lastCandle[5] = lastTrade[2]
+
+                    // console.log("merge candle", lastCandle)
+
+                    this.chart.update({
+                        'candle': lastCandle
+                    })
+                } catch (error) {
+                    console.log(error);
+                }
+            }, 1000)
         }
+
+    },
+    mounted() {
+        window.addEventListener('resize', this.onResize)
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize)
+    },
+    created() {
+        this.fetchHistoricalData();
     },
     data() {
         return {
+            tf: '1m',
             chart: new DataCube({
-                chart: {
-                    type: "Candles",
-                    data:[
-                        [1652638440000,29976,29991,29972,29989,162186.0254],
-                        [1652638380000,29941,29978,29941,29976,262190.274],
-                        [1652638320000,29970,29970,29924,29941,367089.0866],
-                        [1652638260000,29981,29981,29967,29970,433508.9222],
-                        [1652638200000,29959,29986,29959,29981,467224.9679],
-                        [1652638140000,29960,29962,29956,29959,57822.861]
-                    ]
-                }
-                // onchart: [
-                //     {
-                //         name: "Trades",
-                //         type: "Trades",
-                //         data:[
-                //             [1651676702000,1,38780],
-                //             [1651677276000,0,38760],
-                //             [1651773584000,0,36800],
-                //             [1651773784000,0,36800],
-                //         ],
-                //         settings: {
-                //             "legend": true,
-                //             "z-index": 1,
-                //             showLabel: true,
-                //             markerSize: 20
-                //         }
-                //     }
-                // ]
+                chart: {},
+                onchart: []
             }),
             width: window.innerWidth,
             height: window.innerHeight,
             colors: {
-                colorBack: "#fff",
-                colorGrid: "#eee",
-                colorText: "#333",
-            },
-        };
-    },
-    methods: {
-        onResize(event) {
-            this.width = window.innerWidth;
-            this.height = window.innerHeight;
+                colorBack: '#fff',
+                colorGrid: '#eee',
+                colorText: '#333',
+            }
         }
-        // async fetchOrderHistory() {
-        //     try {
-        //         let rawResponse = await fetch("//localhost:3000/api/v1/orderhistory");
-        //         let response = await rawResponse.json()
-        //         console.log("historicaldata", response.status)
-        //         if (response.status == 'success') {
-        //             this.data.set('onchart.0.data', resp.data)
-        //         } else {
-        //             console.log("fetchOrderHistory.FAILED")
-        //         }
-        //     } catch (error) {
-        //         console.log(error);
-        //     }
-        // },
-        // async fetchHistoricalData() {
-        //     try {
-        //         let rawResponse = await fetch("//localhost:3000/api/v1/historicaldata");
-        //         let response = await rawResponse.json()
-        //         console.log("historicaldata", response.status)
-        //         if (response.status == 'success') {
-        //             // console.log(response.data);
-        //             // this.data.set('chart.data', response.data)
-        //             // this.updateTicker();
-        //             // this.fetchOrderHistory()
-        //         } else {
-        //             console.log("fetchHistoricalData.FAILED")
-        //         }
-        //     } catch (error) {
-        //         console.log(error);
-        //     }
-        // },
-        // async updateTicker() {
-        //     setInterval(async () => {
-        //         try {
-        //             let rawResponse = await fetch("//localhost:3000/api/v1/lastcandle");
-        //             var response = await rawResponse.json()
-        //             console.log("lastcandle: ", response.status)
-        //             if (response.status == 'success') {
-        //                 // console.log(trade)
-        //                 let ohlcv = this.data.data.chart.data
-        //                 let last = ohlcv[ohlcv.length - 1]
-        //                 console.log("last")
-        //                 console.log(last)
-        //                 this.data.update({
-        //                     price:  response.data[0]["close"],
-        //                     volume: response.data[0]["volume"]
-        //                     // candle: [
-        //                     //   tick[0],
-        //                     //   tick[1],
-        //                     //   tick[2],
-        //                     //   tick[3],
-        //                     //   tick[4],
-        //                     //   tick[5]
-        //                     // ],
-        //                     // 'EMA': 8576, // query => value
-        //                     // 'BB': [8955, 8522] // query => [value, value, ...]
-        //                 })
-        //             } else {
-        //                 console.log("updateTicker.FAILED")
-        //             }
-        //         } catch (error) {
-        //             console.log(error);
-        //         }
-        //     }, 3000)
-        // }
-    },
-    mounted() {
-        window.addEventListener("resize", this.onResize);
-    },
-    beforeDestroy() {
-        window.removeEventListener("resize", this.onResize);
-    },
-    created() {
-        // this.fetchHistoricalData();
-    },
-};
+    }
+}
 </script>
